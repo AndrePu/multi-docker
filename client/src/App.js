60d2b352d2fc4d33ffa076efb91ad3622af4b2c8
AndrePu <pugach@dlit.dp.ua>
2025-07-06 20:43:05 +0000

    Change header

--- client/src/App.js
+++ client/src/App.js
@@ -11,10 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
-          <h1>
-            Fibonacci Calculator
-          </h1>
+          <h1 class="App-header"> Fibonacci Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
